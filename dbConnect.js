da3fc419aa10163e0af63b5aee85fbd0e177753c
Praman Singh Tomar <pramantomar@gmail.com>
2023-11-16 18:36:55 +0530

    First Commit

---
+++ dbConnect.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+
+dotenv.config('./.env');
+
+const mongooseUri = process.env.MONGOOSE_URL;
+
+export default () => {
+    try {
+        mongoose.connect(mongooseUri,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            },
+        ).then(console.log("MongoDB Connected"));
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
